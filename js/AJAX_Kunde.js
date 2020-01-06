7b54cffdb1f3c7c5b8733d877517a38e16ea5787
Marwan B <marwan.bayoumi@gmail.com>
2020-01-06 18:44:29 +0100

    general clean up�

---
+++ js/AJAX_Kunde.js
@@ -0,0 +1,24 @@
+//AJAX request für Kunde
+setInterval(function AJAX_PLS() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            let arr = (response[parseInt(document.querySelector('[data-bestellungID]').dataset.bestellungid)]);
+            let status_of_pizzas = document.querySelectorAll('.status');
+
+            for (let i = 0; i < arr.length; i++) {
+                let ajax_status = JSON.parse(arr[i]);
+                console.dir(ajax_status);
+                for (pizza of status_of_pizzas) {
+                    console.log(pizza.id);
+                    if (pizza.id == ajax_status.PizzaID) {
+                        document.getElementById(pizza.id).innerText = ajax_status.Status;
+                    }
+                }
+            }
+        }
+    }
+    xmlhttp.open("GET", "KundenStatus.php", true);
+    xmlhttp.send();
+}, 3000);
---
+++ js/randoms.js
@@ -0,0 +1,51 @@
+let del = document.querySelector('#delete');
+let inputs = document.querySelectorAll('[id^="pizza-"]');
+let order = document.querySelector('#order');
+let pizzaImages = document.querySelectorAll('.pizzaImage');
+let captions = document.querySelectorAll('figcaption');
+let pizza_list = {};
+let anzahl = {};
+
+
+del.addEventListener("click", function () {
+    inputs.forEach(e => e.value = "0");
+    document.querySelector('#address').value = "";
+    document.querySelector('#sum').innerText = "0";
+});
+
+
+// PizzaID as key for obj
+for (let i = 0; i < captions.length; i++) {
+    let temp = captions[i].innerText.split(' ');
+    let property_id = pizzaImages[i].id;
+    let value = temp[2];
+    let pizza = {};
+
+    pizza[property_id] = value; //{pizzaID: pizzaPrice}
+    pizza_list[property_id] = pizza; // list of pizzas
+}
+
+
+
+for (test of pizzaImages) {
+    test.addEventListener("click", function () {
+        let th = parseFloat(document.querySelector('#sum').innerText);
+        let element_id = this.id;
+        let input_var = document.getElementById('pizza-' + element_id);
+        if (input_var.value !== 0 ) {
+            anzahl[element_id] = parseInt(input_var.value);
+            anzahl[element_id] += 1;
+            input_var.value = anzahl[element_id];
+            th += parseFloat(pizza_list[element_id][element_id]);
+            let the = document.querySelector('#sum');
+            the.innerText = th.toFixed(2);
+        } else {
+            anzahl[element_id] = 1;
+            input_var.value = anzahl[element_id];
+            th += parseFloat(pizza_list[element_id][element_id]);
+            let the = document.querySelector('#sum');
+            the.innerText = th.toFixed(2);
+        }
+    })
+}
+
